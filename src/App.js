ace11692a4d07369e206d00440ebec3f210616d5
Subramanyanayak <kssubramanyanayak1997@gmail.com>
2021-03-26 22:32:26 +0530

    adding menus functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './Components/Header'
 import Menu from './Components/menu'
 import Editor from './Components/editor'
+//import Demomenu from './Components/demomenu'
 function App() {
   return (
     <div className="App">
--- src/Components/menu.js
+++ src/Components/menu.js
@@ -1,10 +1,47 @@
 import React from 'react'
 import '../../src/menu.css';
+import {v4 as uuid} from 'uuid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { faExpand } from '@fortawesome/free-solid-svg-icons'
+import {useState} from 'react';
 
 function Menu() {
+
+  const [menu,setMenu] = useState([]);
+  const [items,setItems] = useState({key: '', value: ''});
+  const [submenu,setsubmenu] = useState([]);
+  const [submenuitems,setSubmenuitems] = useState({key: '', value: ''})
+  const [clickid,setClickid] = useState('');
+  const [subclickid,setSubclickid] = useState('');
+
+  let handleChange = (e) => {
+    setItems({ ...items, value: e.target.value})
+  }
+
+  let handleChange2 = (e) => {
+    setSubmenuitems({ ...submenuitems, value: e.target.value})
+  }
+
+  let handleClick = () => {
+   var id = uuid();
+    setItems({ ...items, key: id});
+    setMenu([ ...menu, items]);
+    console.log(menu);
+  }
+
+  let handleClick2 = (id) => {
+   setSubclickid(id);
+    setSubmenuitems({ ...submenuitems, key: id});
+    setsubmenu([ ...submenu, submenuitems]);
+    console.log(submenu);
+    console.log(subclickid);
+  }
+
+let viewMore = (id) => {
+  setClickid(id);
+}
+
   return (
     <div className="menu">
         <nav>
@@ -16,12 +53,26 @@ function Menu() {
             </ul>
         </nav>
         <div className="menucontainer">
+          <input type="text" onChange={handleChange2}></input>
         <div className="addmenu">
-        <input type="text" placeholder="DFIN" className="menuinput"></input>
-        <FontAwesomeIcon icon={faPlus} className="faplus"/>
+        <input type="text" onChange={handleChange} placeholder="DFIN" className="menuinput"></input>
+        <FontAwesomeIcon icon={faPlus} className="faplus" onClick={handleClick}/>
         <FontAwesomeIcon icon={faExpand} className="faexpand"/>
         <FontAwesomeIcon icon={faArrowLeft} className="faarrowleft"/>
         </div>
+        <ul>
+        {menu.map(obj => (
+          <div className="container">
+          <li className="eachrow"><a href="#" onClick={viewMore.bind(this, obj.key)}>{obj.value}</a></li> 
+        {obj.key == subclickid? 
+        submenu.map(obj2 => (
+           subclickid == obj2.key ?
+            <li className="value">{obj2.value}</li> : null 
+        )): null }
+          <FontAwesomeIcon icon={faPlus} className="faplus2" onClick={handleClick2.bind(this, obj.key)}/>
+        </div>
+        ))}
+        </ul>
         </div>
     </div>
   );
